aa479bab111847c72f0aec96c8bd7201357f5000
Osama Yawar <osama.yawar@ntgfreight.com>
2022-01-07 10:18:37 -0500

    Moved the logic for users crud operation inside users controller

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,16 @@
 import express from 'express';
-import { v4 as uuidv4 } from 'uuid';
+import { getUsers, createUser, getUser, deleteUser, updateUser } from '../controllers/users.js';
 
 const router = express.Router();
-let users = [];
 
-router.get('/', (req, res) => {
-  res.send(users);
-});
+router.get('/', getUsers);
 
-router.post('/', (req, res) => {
-  const newUsers = req.body;
-  newUsers.forEach(user => {
-    users.push({ ...user, "id": uuidv4() });
-  });
-  res.send(`Users successfully added`);
-});
+router.post('/', createUser);
 
-router.get('/:id', (req, res) => {
-  const user = users.find((user) => user.id == req.params.id);
-  res.send(user);
-});
+router.get('/:id', getUser);
 
-router.delete('/:id', (req, res) => {
-  users = users.filter((user) => user.id !== req.params.id);
-  res.send(`User with id: ${req.params.id} has been successfully deleted`);
-});
+router.delete('/:id', deleteUser);
+
+router.patch('/:id', updateUser);
 
 export default router;
